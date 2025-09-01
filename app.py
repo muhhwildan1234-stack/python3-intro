a355600754f4b414172fdee83c46a5071bd426bb
wldanresingg <muhhwildan123@gmail.com>
2025-09-01 21:35:21 +0200

    add app file

---
+++ app.py
@@ -0,0 +1,10 @@
+print("BELAJAR DASAR GIT")
+
+# git add [nama_file_yang_berubah]
+# git commit -m "isi_pesan_perubahan"
+# git push -u origin main
+
+# ----cara tambah app .py file----
+# git add app.py
+# git commit -m "add app file "
+# git push -u origin main
